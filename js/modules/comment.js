742dd1999d3dfed5c6c6a84d16e943da99a5d8e1
李叶 <liye@liyedeMacBook-Pro.local>
2017-05-12 15:10:34 +0800

    fix comment

--- js/modules/comment.js
+++ js/modules/comment.js
@@ -4,28 +4,28 @@
 
 comments = [
     {
-        "content": "This is the best site I've ever seen.",
+        "content": "This is the best site I've ever seen, excellent, perfect.",
         "name": "李美丽",
         "time": "14:23pm, 12 Jun 2017"
     },
     {
-        "content": "你形容我是这个世界上无与伦比的美丽.",
-        "name": "网红欣",
+        "content": "好累，快被打败了",
+        "name": "恬恬在奔跑",
         "time": "14:25pm, 12 Jun 2017"
     },
     {
-        "content": "<i class='fa fa-hand-peace-o'><i class='fa fa-hand-peace-o'>完美",
+        "content": '好甜 <i class="fa fa-heart-o"></i>',
         "name": "雪碧",
         "time": "15:11pm, 12 Jun 2017"
     },
     {
-        "content": "This is the best site I've ever seen.",
+        "content": '回答在牛奶箱里 <i class="fa fa-envelope-open-o"></i>',
         "name": "申十七",
         "time": "15:45pm, 12 Jun 2017"
     },
     {
-        "content": "This is the best site I've ever seen.",
-        "name": "北投陈yz",
+        "content": "好漂亮啊。",
+        "name": "抠脚少女陈yz",
         "time": "15:57pm, 12 Jun 2017"
     }
 ];
@@ -37,3 +37,18 @@ comment_pic = [
     "http://opmpd352q.bkt.clouddn.com/comment_1.jpg",
     "http://opmpd352q.bkt.clouddn.com/comment_2.jpg"
 ];
+
+function init_comment(){
+    var comment_html = "";
+    for (var i=0; i<comments.length; i++){
+        comment_html += '<article class="comment">' +
+            '<div class="comment-img"><img src="' + comment_pic[i%5] + '" width="50" height="50"></div>' +
+            '<div class="comment-body">' +
+            '<div class="text"><p>' + comments[i].content + '</p></div>' +
+            '<p class="attribution">by <mark>' + comments[i].name + '</mark> at ' + comments[i].time + '</p>' +
+            '</div></article>';
+    }
+    return comment_html;
+}
+
+$("#comment-section").html(init_comment());
